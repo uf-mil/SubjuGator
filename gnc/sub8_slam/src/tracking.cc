44676a8ca6b0e22aa749ffb5feca7a93f42dc883
Jacob Panikulam <jpanikulam@ufl.edu>
2016-01-04 23:32:10 -0500

    SLAM: Add get_points

--- gnc/sub8_slam/src/tracking.cc
+++ gnc/sub8_slam/src/tracking.cc
@@ -1,7 +1,4 @@
-#include <vector>
 #include <string>
-#include <iostream>
-#include <opencv2/opencv.hpp>
 #include <sub8_slam/slam.h>
 
 // #define UNDISTORT
@@ -23,6 +20,32 @@ void preprocess(cv::Mat& input_image, cv::Mat& output_image, const cv::Mat& intr
 #endif
 }
 
+void initialize(const cv::Mat& frame, PointVector& corners, IdVector& feature_ids) {
+  // Parameters to twiddle
+  // These should NOT be exposed
+  int feature_mask_offset = 5;
+  int max_corners = 2000;
+  double quality = 0.25;
+  double min_distance = 10;
+  int block_size = 7;      // 5
+  bool use_harris = true;  // Use harris corner detector?
+
+  // Create feature mask (Only search here for features)
+  cv::Mat feature_mask = cv::Mat(frame.rows, frame.cols, CV_8UC1, 1);
+  // TODO: Make feature mask work
+  // cv::Point2i lower_right(feature_mask_offset, feature_mask_offset);
+  // cv::Point2i upper_right(frame.cols - feature_mask_offset, frame.rows - feature_mask_offset);
+  // cv::rectangle(feature_mask, lower_right, upper_right, cv::Scalar(1));
+
+  // Discover features
+  cv::goodFeaturesToTrack(frame, corners, max_corners, quality, min_distance, feature_mask,
+                          block_size, use_harris);
+
+  for (unsigned int k = 0; k < corners.size(); k++) {
+    feature_ids.push_back(k);
+  }
+}
+
 IdVector which_points(const StatusVector& status, const IdVector& previous) {
   /* Figure out which points from the original appear in the current frame, numbered */
   IdVector point_associations;
@@ -42,34 +65,21 @@ IdVector which_points(const StatusVector& status, const IdVector& previous) {
   return point_associations;
 }
 
-void initialize(const cv::Mat& frame, PointVector& corners, IdVector& feature_ids) {
-  // Parameters to twiddle
-  // These should NOT be exposed
-  int feature_mask_offset = 5;
-  int max_corners = 2000;
-  double quality = 0.25;
-  double min_distance = 7;
-  int block_size = 5;      // 5
-  bool use_harris = true;  // Use harris corner detector?
-
-  // Create feature mask (Only search here for features)
-  cv::Mat feature_mask = cv::Mat(frame.rows, frame.cols, CV_8UC1, 1);
-  // cv::Point2i lower_right(feature_mask_offset, feature_mask_offset);
-  // cv::Point2i upper_right(frame.cols - feature_mask_offset, frame.rows - feature_mask_offset);
-  // cv::rectangle(feature_mask, lower_right, upper_right, cv::Scalar(1));
-
-  // Discover features
-  cv::goodFeaturesToTrack(frame, corners, max_corners, quality, min_distance, feature_mask,
-                          block_size, use_harris);
-
-  for(unsigned int k=0; k < corners.size(); k++) {
-    feature_ids.push_back(k);
+Point3Vector get_points(const IdVector& keep_ids, const Point3Vector& points) {
+  /* This is wasteful (making a new vector each time!)
+    TODO: Make our own pnp solver that works with pt masks (No copies)
+  */
+  // TODO Make this and filter into a template
+  Point3Vector output_points;
+  for (unsigned int k = 0; k < keep_ids.size(); k++) {
+    // Don't waste cycles with checks
+    output_points.push_back(points[k]);
   }
+  return output_points;
 }
 
 PointVector filter(const StatusVector& status, const PointVector& points) {
-  /* filtered_points should be empty
-
+  /* Return a PointVector where the elements for which status=0 are removed from points.
   */
   PointVector filtered_points;
   for (unsigned int k = 0; k < status.size(); k++) {
@@ -96,21 +106,6 @@ void optical_flow(const cv::Mat& prev_frame, const cv::Mat& cur_frame, PointVect
   std::vector<float> error;
   cv::calcOpticalFlowPyrLK(prev_frame, cur_frame, prev_pts, next_pts, status, error, win_size,
                            max_level, criteria);
-}
-
-void draw_points(cv::Mat& frame, const PointVector& points) {
-  int radius = 5;
-  for (unsigned int k = 0; k < points.size(); k++) {
-    cv::circle(frame, points[k], radius, cv::Scalar(240, 0, 0), 1);
-    // todo: red rectangle outlines would look cooler
-  }
-}
-
-void draw_point_ids(cv::Mat& frame, const PointVector& points, const IdVector& point_ids) {
-  for (unsigned int k = 0; k < points.size(); k++) {
-    // cv::circle(frame, points[k], radius, cv::Scalar(240), 1);
-    cv::putText(frame, std::to_string(point_ids[k]), points[k], cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 250, 0));
-  }
 }
 
 }
