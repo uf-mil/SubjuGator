a3edc17db47625dd11952cb3a159ae5bf62af658
RustyBamboo <dvolya@gmail.com>
2017-07-27 18:58:22 -0400

    NAV_GATE: follow normal vector in mission

--- command/sub8_missions/missions/navigation_gate.py
+++ command/sub8_missions/missions/navigation_gate.py
@@ -17,7 +17,7 @@ fprint = text_effects.FprintFactory(
 
 # Distance before and after the gate in meters
 FACTOR_DISTANCE_BEFORE = 1.5
-FACTOR_DISTANCE_AFTER = 1.5
+FACTOR_DISTANCE_AFTER = 7.5
 
 SPEED = 0.3
 
@@ -25,7 +25,8 @@ SPEED = 0.3
 @txros.util.cancellableInlineCallbacks
 def run(sub):
     yield sub.vision_proxies.navigation_gate.start()
-
+    fprint("Moving down to depth")
+    yield sub.move.depth(2).go()
     # Add search pattern if needed...
     search_pattern = [sub.move.yaw_left(0.261799),
                       sub.move.yaw_right(0.261799 * 2),
@@ -38,7 +39,7 @@ def run(sub):
 
     resp = None
     fprint('Searching...')
-    resp = yield search.start_search(loop=False, timeout=100, spotings_req=1)
+    resp = yield search.start_search(loop=False, timeout=100, spotings_req=5, speed=0.3)
 
     if resp is None or not resp.found:
         fprint("No gate found...", msg_color="red")
@@ -50,20 +51,32 @@ def run(sub):
     fprint('Gate\'s position in map is: {}'.format(position))
     fprint('Gate\'s orientation in map is: {}'.format(orientation))
 
-    # Get the normal vector, which is assumed to be the [1,0,0] unit vector
-    normal = tf.transformations.quaternion_matrix(
-        orientation).dot(np.array([1, 0, 0, 0]))[0:3]
-    fprint('Computed normal vector: {}'.format(normal))
+    point_before = 0
+    point_after = 0
 
-    # Computer points before and after the gate for the sub to go to
-    point_before = position + FACTOR_DISTANCE_BEFORE * normal
-    point_after = position - FACTOR_DISTANCE_AFTER * normal
+    distance = np.linalg.norm(sub.pose.position - position)
+    while (distance > 1 or resp is not None or resp.found):
 
-    # go in front of gate
-    fprint('Moving infront of gate {}'.format(point_before))
-    yield sub.move.set_position(point_before).look_at(point_after).zero_roll_and_pitch().go(speed=SPEED)
+        # Get the normal vector, which is assumed to be the [1,0,0] unit vector
+        normal = tf.transformations.quaternion_matrix(
+            orientation).dot(np.array([1, 0, 0, 0]))[0:3]
+        fprint('Computed normal vector: {}'.format(normal))
 
-    # go through the gate
+        # Computer points before and after the gate for the sub to go to
+        point_before = position + FACTOR_DISTANCE_BEFORE * normal
+        point_after = position - FACTOR_DISTANCE_AFTER * normal
+
+        # go in front of gate
+        fprint('Moving infront of gate {}'.format(point_before))
+        yield sub.move.set_position(point_before).look_at(point_after).zero_roll_and_pitch().go(speed=SPEED)
+        distance = np.linalg.norm(sub.pose.position - position)
+
+        resp = yield sub.vision_proxies.navigation_gate.get_pose()
+
+        position = rosmsg_to_numpy(resp.pose.pose.position)
+        orientation = rosmsg_to_numpy(resp.pose.pose.orientation)
+
+        # go through the gate
     fprint('YOLO! Going through gate {}'.format(point_after))
     yield sub.move.set_position(point_after).zero_roll_and_pitch().go(speed=SPEED)
 
