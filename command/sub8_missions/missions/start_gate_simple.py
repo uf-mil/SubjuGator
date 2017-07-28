014ae89128379311659b049f4cc3336277b35ab3
Kevin Allen <kma1660@gmail.com>
2017-07-27 23:59:46 -0700

    START GATE SIMPLE: increase distance, lower speed

--- command/sub8_missions/missions/start_gate_simple.py
+++ command/sub8_missions/missions/start_gate_simple.py
@@ -1,12 +1,12 @@
 from txros import util
 from mil_misc_tools import text_effects
 
-FORWARD_METERS = 3.0
-
+FORWARD_METERS = 15.0
+SPEED=0.5
 
 @util.cancellableInlineCallbacks
 def run(sub):
     fprint = text_effects.FprintFactory(title='START_GATE (SIMPLE)').fprint
     fprint('Going forward')
-    yield sub.move.zero_roll_and_pitch().forward(FORWARD_METERS).go()
+    yield sub.move.zero_roll_and_pitch().forward(FORWARD_METERS).go(speed=SPEED)
     fprint('Done!')
