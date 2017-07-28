c23995f8b3d5dde3405200828cf8a63564fd3f88
Kevin Allen <kma1660@gmail.com>
2017-07-27 23:46:14 -0700

    AUTONOMOUS: add short mission to test network-kill by publishing wrench

---
+++ command/sub8_missions/missions/network_loss_test.py
@@ -0,0 +1,23 @@
+from txros import util
+import genpy
+from geometry_msgs.msg import WrenchStamped
+
+@util.cancellableInlineCallbacks
+def run(sub):
+    '''
+    Short mission to pubish a forward wrench, can be used to test
+    network kill out of water (first kill the controller, call this mission
+    from autonomous mission)
+    '''
+    wrench = WrenchStamped()
+    wrench.header.frame_id = '/base_link'
+    wrench.header.stamp = sub.nh.get_time()
+    wrench.wrench.force.x = 5.0
+    pub = sub.nh.advertise('/wrench', WrenchStamped)
+    yield sub.nh.sleep(1.0)
+    print "Going"
+    start = sub.nh.get_time()
+    end = sub.nh.get_time() + genpy.Duration(3.0)
+    while sub.nh.get_time() < end:
+        pub.publish(wrench)
+        yield sub.nh.sleep(0.1)
